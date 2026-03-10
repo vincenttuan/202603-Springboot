010d01f00c44472ac76118bd0b606817f1e0cf4e
User <User@host.docker.internal>
2026-03-10 20:42:15 +0800

    spring

---
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 可以省去撰寫 @ResponseBody
+public class ApiController {
+	
+	/**
+	 * 1. API 首頁
+	 * 路徑1: /home
+	 * 路徑2: /welcome
+	 * 網址1: http://localhost:8080/home
+	 * 網址2: http://localhost:8080/welcome
+	 * */
+	@GetMapping(value = {"/home", "/welcome"})
+	public String home() {
+		return "我是 API 首頁";
+	}
+	
+}
