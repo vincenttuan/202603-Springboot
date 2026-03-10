bc810ae1bde46ba404d93fb56c69a3066485d8f4
User <User@host.docker.internal>
2026-03-10 21:30:03 +0800

    spring

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,19 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 可以省去撰寫 @ResponseBody
+@RequestMapping("/api")
 public class ApiController {
 	
 	/**
 	 * 1. API 首頁
 	 * 路徑1: /home
-	 * 網址1: http://localhost:8080/home
+	 * 網址1: http://localhost:8080/api/home
 	 * 
 	 * 路徑2: /welcome
-	 * 網址2: http://localhost:8080/welcome
+	 * 網址2: http://localhost:8080/api/welcome
 	 * */
 	@GetMapping(value = {"/home", "/welcome"}, produces = "text/plain;charset=utf-8")
 	public String home() {
@@ -23,11 +25,11 @@ public class ApiController {
 	/**
 	 * 2. ?帶參數
 	 * 路徑1: /greet?name=John&age=18
-	 * 網址1: http://localhost:8080/greet?name=John&age=18
+	 * 網址1: http://localhost:8080/api/greet?name=John&age=18
 	 * 結果1: Hi John, 18(成年)
 	 * 
 	 * 路徑2: /greet?name=Mary
-	 * 網址2: http://localhost:8080/greet?name=Mary
+	 * 網址2: http://localhost:8080/api/greet?name=Mary
 	 * 結果2: Hi Mary, 0(未成年)
 	 * 
 	 * 限制: name 為必要參數, age 為可選參數(有初始值 0)
