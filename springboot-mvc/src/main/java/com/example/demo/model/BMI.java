451ccf3b9c2e8fc37ef1a407c514814578d5f2f1
User <User@host.docker.internal>
2026-03-12 19:16:06 +0800

    bmi

---
+++ springboot-mvc/src/main/java/com/example/demo/model/BMI.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BMI {
+	private Double height;
+	private Double weight;
+	private Double bmi;
+	
+}
