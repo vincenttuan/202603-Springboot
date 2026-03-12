4743d8defccd861d04569d4a96944d4f18601ad4
User <User@host.docker.internal>
2026-03-12 20:19:17 +0800

    api response

--- springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
+++ springboot-mvc/src/main/java/com/example/demo/response/ApiResponse.java
@@ -10,4 +10,14 @@ import lombok.NoArgsConstructor;
 public class ApiResponse<T> {
 	private String message; // 訊息 例如:查詢成功, 新增成功, 新增失敗 ...
 	T data; // payload 實際資料
+	
+	// 成功回應
+	public static <T> ApiResponse<T> success(String message, T data) {
+		return new ApiResponse<T>(message, data);
+	}
+	
+	// 失敗回應
+	public static <T> ApiResponse<T> error(String message) {
+		return new ApiResponse<T>(message, null);
+	}
 }
