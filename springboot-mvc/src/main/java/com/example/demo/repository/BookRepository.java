4856a84bca24efc2a2f403fca1e14102c4cce2e9
User <User@host.docker.internal>
2026-03-17 21:09:13 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/repository/BookRepository.java
+++ springboot-mvc/src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,5 +1,25 @@
 package com.example.demo.repository;
 
-public interface BookRepository {
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.model.Book;
+
+public interface BookRepository {
+	
+	// 查找全部書籍
+	List<Book> findAllBooks();
+	
+	// 查找指定書籍
+	Optional<Book> getBookById(Integer id);
+	
+	// 新增書籍
+	boolean addBook(Book book);
+	
+	// 修改書籍
+	boolean updateBook(Integer id, Book book);
+	
+	// 刪除書籍
+	boolean deleteBook(Integer id);
+	
 }
