982a118eda07e47cea4dd5bb7038929c0021b48e
User <User@host.docker.internal>
2026-03-19 19:14:38 +0800

    book

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.response.ApiResponse;
+import com.example.demo.service.BookService;
 
 /**
  * BookController
@@ -81,4 +90,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/book")
 public class BookController {
 	
+	@Autowired
+	//@Qualifier("bookServiceImpl") // 若該 interface 只有一個實現類,則 @Qualifier 可以省略
+	private BookService bookService;
+	
+	public ResponseEntity<ApiResponse<List<Book>>> findAllBooks() {
+		List<Book> books = bookService.findAllBooks();
+		if(books.size() == 0) {
+			return ResponseEntity.badRequest().body(ApiResponse.error("查無任何書籍"));
+		}
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", books));
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
