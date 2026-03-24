66814cb75b61fc100219d80ba0cbf05fb78e9033
User <User@DESKTOP-FM2EGTO>
2026-03-24 21:52:01 +0800

    book

--- springboot-mvc/src/main/resources/static/js/book.js
+++ springboot-mvc/src/main/resources/static/js/book.js
@@ -49,16 +49,21 @@ async function handleResponse(response) {
 	return result;
 }
 
+// 表單新增的資料
+function getBookFormDataForCreate() {
+	return 	{
+		name: bookName.value.trim(),
+		price: bookPrice.value ? Number(bookPrice.value) : null,
+		amount: bookAmount.value ? Number(bookAmount.value) : null,
+		pub: bookPub.checked
+	};
+}
+
 // 新增書籍
 async function addBook() {
 	try {
 		
-		const book = {
-			name: bookName.value.trim(),
-			price: bookPrice.value ? Number(bookPrice.value) : null,
-			amount: bookAmount.value ? Number(bookAmount.value) : null,
-			pub: bookPub.checked
-		};
+		const book = getBookFormDataForCreate();
 		
 		if(!book.name || !book.price === null || book.amount === null) {
 			showMessage("請輸入完整書名, 價格, 數量", "error");
