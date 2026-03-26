9f4e10bd04e740764726e4d1ac27259547986f20
User <User@host.docker.internal>
2026-03-26 20:18:53 +0800

    book

--- springboot-mvc/src/main/resources/static/js/book.js
+++ springboot-mvc/src/main/resources/static/js/book.js
@@ -231,6 +231,7 @@ function renderBookTable(books) {
 				<td>${book.pub ? "是" : "否"}</td>
 				<td>
 					<button onclick="loadBookById(${book.id})">修改</button>
+					<button class="danger">刪除</button>
 				</td>
 			</tr>
 		`;
