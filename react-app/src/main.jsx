86c80e45345d45d89f6cc7a343a943afcc7f7402
VincentTuan <vincentjava@yahoo.com.tw>
2026-03-31 21:39:44 +0800

    Add files via upload

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -6,7 +6,8 @@ import ReactDOM from 'react-dom/client'
 //import App from './App4.jsx'
 //import App from './App5.jsx'
 //import App from './App6.jsx'
-import App from './App7.jsx'
+//import App from './App7.jsx'
+import App from './App8.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
