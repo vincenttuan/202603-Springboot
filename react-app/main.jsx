2ebd12dbc8249819f6c297bf90ee82926504ad52
VincentTuan <vincentjava@yahoo.com.tw>
2026-04-02 20:05:11 +0800

    Add files via upload

---
+++ react-app/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+//import App from './App2.jsx'
+//import App from './App3.jsx'
+//import App from './App4.jsx'
+//import App from './App5.jsx'
+//import App from './App6.jsx'
+//import App from './App7.jsx'
+//import App from './App8.jsx'
+//import App from './App9.jsx'
+//import App from './App10.jsx'
+//import App from './App11.jsx'
+//import App from './App12.jsx'
+import App from './App13.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
