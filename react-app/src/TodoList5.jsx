1efb25416404b87ed3928a5b0557125188869c77
VincentTuan <vincentjava@yahoo.com.tw>
2026-04-07 20:07:17 +0800

    Add files via upload

---
+++ react-app/src/TodoList5.jsx
@@ -0,0 +1,58 @@
+// TodoList 練習 5
+import './App.css'
+import { useState } from 'react'
+
+function App () {
+
+    const [todos, setTodos] = useState([
+        {id:1, text:'吃早餐', completed:true},
+        {id:2, text:'做體操', completed:false},
+        {id:3, text:'寫程式', completed:false},
+    ])
+    const [todo, setTodo] = useState('')
+
+    // 變更 todo 資料
+    const handleTodoChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    // 加入 todo 到 todos
+    const handleTodoAdd = (e) => {
+        //const newId = todos.length > 0 ? todos.length + 1 : 1;
+        // 在 todos 中找到最大值後 + 1
+        const newId = todos.length > 0 ? Math.max(...todos.map((t) => t.id)) + 1 : 1;
+        const newTodo = {id:newId, text:todo, completed:false};
+        setTodos([...todos, newTodo]);
+        setTodo('');
+    }
+
+    const toggleCompletion = (id) => {
+        // 透過 setTodos 來變更 todos 的 completed 資料
+        // {id:1, text:'吃早餐', completed:true}
+        setTodos(
+            todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo )
+        );
+    }
+
+    return (
+        <>
+            <h1>My TodoList 5</h1>
+            <div>
+                <input type='text' value={todo} onChange={handleTodoChange} />
+                <button onClick={handleTodoAdd}>加入</button>
+            </div>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li key={todo.id} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
+                            {todo.id} - {todo.text}
+                            <input type="checkbox" checked={todo.completed} onChange={() => toggleCompletion(todo.id)} />
+                        </li>
+                    ))    
+                }
+            </ul>
+        </>
+    )
+}
+
+export default App
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -19,7 +19,8 @@ import ReactDOM from 'react-dom/client'
 //import App from './TodoList.jsx'
 //import App from './TodoList2.jsx'
 //import App from './TodoList3.jsx'
-import App from './TodoList4.jsx'
+//import App from './TodoList4.jsx'
+import App from './TodoList5.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
