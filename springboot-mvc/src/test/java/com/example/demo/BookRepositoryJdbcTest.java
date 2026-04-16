b3936d794944a684db943b9fff0a631fa596323f
User <User@host.docker.internal>
2026-04-16 21:16:38 +0800

    web

--- springboot-mvc/src/test/java/com/example/demo/BookRepositoryJdbcTest.java
+++ springboot-mvc/src/test/java/com/example/demo/BookRepositoryJdbcTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,21 @@ public class BookRepositoryJdbcTest {
 	@Qualifier("bookRepositoryJdbcImpl") // 指定實現類
 	private BookRepository bookRepository;
 	
-	@Test
+	//@Test
 	void queryTest() {
 		List<Book> books = bookRepository.findAllBooks();
 		books.forEach(System.out::println);
 	}
+	
+	@Test
+	void getTest() {
+		Optional<Book> optBook = bookRepository.getBookById(1);
+		if(optBook.isPresent()) {
+			Book book = optBook.get();
+			System.out.println(book);
+		} else {
+			System.out.println("查無資料");
+		}
+	}
+	
 }
