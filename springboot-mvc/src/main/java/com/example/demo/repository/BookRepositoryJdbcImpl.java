d3727c53590aef07a24f161d1809c2073c0be6ba
User <User@host.docker.internal>
2026-04-16 20:33:34 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/repository/BookRepositoryJdbcImpl.java
+++ springboot-mvc/src/main/java/com/example/demo/repository/BookRepositoryJdbcImpl.java
@@ -1,5 +1,49 @@
 package com.example.demo.repository;
 
-public class BookRepositoryJdbcImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.Book;
+
+@Repository
+public class BookRepositoryJdbcImpl implements BookRepository {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate; // 自動綁定 spring 的 JdbcTemplete 物件 
+	
+	@Override
+	public List<Book> findAllBooks() {
+		String sql = "select id, name, price, amount, pub from book";
+		// 利用 BeanPropertyRowMapper(Book.class) 會自動將資料表中查到的的每一筆紀錄注入到 Book 物件中
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class));
+	}
+
+	@Override
+	public Optional<Book> getBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean addBook(Book book) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateBook(Integer id, Book book) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteBook(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
