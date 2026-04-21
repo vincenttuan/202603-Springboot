3eb7bcff2ec152576e1a02f0ea2a0e9cc02d62e1
User <User@host.docker.internal>
2026-04-21 21:48:24 +0800

    ssr

--- springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/SSRBookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,5 +64,19 @@ public class SSRBookController {
 		
 		return "redirect:/ssr/book";
 	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteBook(@PathVariable Integer id, Model model) {
+		try {
+			bookService.deleteBook(id);
+		} catch (BookException e) {
+			model.addAttribute("message", e.getMessage());
+			return "book/error";
+		}
+		
+		return "redirect:/ssr/book";
+	}
+	
+	
 	
 }
