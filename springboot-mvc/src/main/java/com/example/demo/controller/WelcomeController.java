68ec28cba066eb04b1ff8b4b1d93dca1ff61b019
User <User@host.docker.internal>
2026-04-21 19:51:50 +0800

    ssr

--- springboot-mvc/src/main/java/com/example/demo/controller/WelcomeController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/WelcomeController.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
+	@GetMapping("/welcome")
 	public String welcome(Model model) {
 		// model 裡面放的就是要傳給 jsp 的資料
 		model.addAttribute("name", "阿山哥");
