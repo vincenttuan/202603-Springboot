28b07ca5167e85721441cb6117b961b64f138fcf
User <User@DESKTOP-FM2EGTO>
2026-04-23 20:58:16 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/repository/StoryBookRepository.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/repository/StoryBookRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.StoryBook;
@@ -13,7 +14,14 @@ public interface StoryBookRepository extends JpaRepository<StoryBook, Integer> {
 	// 依 id 由小到大排序
 	List<StoryBook> findAllByOrderByIdAsc();
 	
-	// 依 id 由大到排序
+	// 依 id 由大到小排序
 	List<StoryBook> findAllByOrderByIdDesc();
 	
+	// 依 id 由小到大排序: T-SQL 版
+	@Query(value = "select * from story_book order by id asc", nativeQuery = true)
+	List<StoryBook> findAllAsc();
+	
+	// 依 id 由大到小排序: JPQL
+	@Query("select s from StoryBook s order by s.id desc")
+	List<StoryBook> findAllDesc();
 }
