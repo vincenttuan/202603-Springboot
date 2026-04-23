34e996bb3cbe38885a1ac019806f8d8635a7a3ba
User <User@DESKTOP-FM2EGTO>
2026-04-23 19:14:01 +0800

    jpa

---
+++ springboot-jpa-storybook/src/main/java/com/example/demo/SpringbootJpaStorybookApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaStorybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaStorybookApplication.class, args);
+	}
+
+}
+
