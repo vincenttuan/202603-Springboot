79dd290b37e3b5a972da4e22c063aa2aba3f5c03
User <User@DESKTOP-FM2EGTO>
2026-04-23 21:43:58 +0800

    jpa

---
+++ springboot-jpa-storybook/src/test/java/com/example/demo/Test_ReadBiography.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Biography;
+import com.example.demo.repository.BiographyRepository;
+
+@SpringBootTest
+public class Test_ReadBiography {
+	
+	@Autowired
+	private BiographyRepository biographyRepository;
+	
+	@Test
+	public void read() {
+		
+		List<Biography> biographies = biographyRepository.findAll();
+		
+		biographies.forEach(bio -> {
+			System.out.printf("id: %d%ntext: %s%nauthor id: %d%nauthor name: %s%n%n",
+					bio.getId(), bio.getText(), bio.getAuthor().getId(), bio.getAuthor().getName());
+			
+		});
+		
+	}
+	
+}
