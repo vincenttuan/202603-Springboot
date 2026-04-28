7887258166532419d4ab81640f9f4a9be4c44e3f
User <User@host.docker.internal>
2026-04-28 20:13:46 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/repository/PublisherRepository.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/repository/PublisherRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface PublisherRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.demo.model.Publisher;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+	
 }
