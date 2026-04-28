8605b9e1bc938bf21d8153ff6ff74439076616dc
User <User@host.docker.internal>
2026-04-28 22:18:26 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkItem.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkItem.java
@@ -1,11 +1,15 @@
 package com.example.demo.model.drink;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -35,5 +39,12 @@ public class DrinkItem {
 	@OneToOne(mappedBy = "drinkItem")
 	private DrinkDetail drinkDetail;
 	
+	@ManyToMany
+	@JoinTable(
+			name = "drink_item_topping",
+			joinColumns = @JoinColumn(name = "drink_item_id"),
+			inverseJoinColumns = @JoinColumn(name = "topping_id")
+	)
+	private List<Topping> toppings;
 	
 }
--- springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/Topping.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/Topping.java
@@ -1,5 +1,32 @@
 package com.example.demo.model.drink;
 
-public class Topping {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Topping {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	// 加料名稱: 珍珠, 椰果, 布丁
+	@Column(length = 50, nullable = false)
+	private String name;
+	
+	// 價料價格
+	@Column(nullable = false)
+	private Integer price;
+	
+	@ManyToMany(mappedBy = "toppings")
+	private List<DrinkItem> drinkItems;
+	
 }
