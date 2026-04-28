fd2ef7e7d93dbbf2a9135d7bcff549f4c9dbf2bf
User <User@host.docker.internal>
2026-04-28 22:07:27 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkDetail.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkDetail.java
@@ -1,5 +1,36 @@
 package com.example.demo.model.drink;
 
-public class DrinkDetail {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Data
+@Entity
+public class DrinkDetail {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = true)
+	// 飲料熱量: 450
+	private Integer calories;
+	
+	@Column(length = 50, nullable = true)
+	// 建議甜度: 半糖, 微糖, 無糖
+	private String sugarSuggestion;
+	
+	@Column(length = 50, nullable = true)
+	// 建議冰塊: 正常冰, 少冰, 去冰
+	private String iceSuggestion;
+	
+	@OneToOne
+	@JoinColumn(name = "drink_item_id")
+	private DrinkItem drinkItem;
+	
 }
