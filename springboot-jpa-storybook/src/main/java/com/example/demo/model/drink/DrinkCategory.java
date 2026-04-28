21f1a1a552d2ecc7e03146a997bc18587897d0fc
User <User@host.docker.internal>
2026-04-28 21:58:09 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkCategory.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkCategory.java
@@ -1,15 +1,18 @@
 package com.example.demo.model.drink;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
-public class DrinkCatagory {
+public class DrinkCategory {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +21,8 @@ public class DrinkCatagory {
 	// 飲料分類名稱:茶類, 咖啡類, 果汁類
 	@Column(length = 50, nullable = false)
 	private String name;
+	
+	@OneToMany(mappedBy = "drinkCategory")
+	private List<DrinkItem> drinkItems;
 	
 }
--- springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkItem.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/drink/DrinkItem.java
@@ -1,5 +1,34 @@
 package com.example.demo.model.drink;
 
-public class DrinkItem {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+//@Table(name = "drink_item")
+public class DrinkItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	// 飲料名稱: 珍珠奶茶, 拿鐵咖啡, 柳橙汁
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	// 飲料基本價格
+	@Column(nullable = false)
+	private Integer price;
+	
+	@ManyToOne
+	@JoinColumn(name = "drink_category_id")
+	private DrinkCategory drinkCategory;
+	
 }
