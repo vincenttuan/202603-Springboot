0ba5aec0307a7d8c83bdb8bdd7942c4b145b9280
User <User@host.docker.internal>
2026-04-28 21:25:08 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/repository/PublisherRepository.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/repository/PublisherRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Publisher;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+	
+	@Modifying
+	@Query(value = "delete from publisher_storybook where publisher_id = :publisher_id and storybook_id = :storybook_id", nativeQuery = true)
+	void deleteBookFromPublisher(@Param("publisher_id") Integer publisherId, @Param("storybook_id") Integer storybookId);
 	
 }
