f23ec5b253377c2df574ff77b2a6a031224b3ef4
User <User@host.docker.internal>
2026-04-28 20:12:49 +0800

    jpa

--- springboot-jpa-storybook/src/main/java/com/example/demo/model/Publisher.java
+++ springboot-jpa-storybook/src/main/java/com/example/demo/model/Publisher.java
@@ -3,13 +3,17 @@ package com.example.demo.model;
 import java.util.List;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import lombok.Data;
 
+@Data
+@Entity
 public class Publisher {
 	
 	@Id
