0045e8063b33de0a4e005da507d4ac9b0fa8041d
User <User@host.docker.internal>
2026-04-28 19:26:50 +0800

    jpa

---
+++ springboot-jpa-storybook/src/test/java/com/example/demo/Test_ReadStoryBook2.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.StoryBook;
+import com.example.demo.repository.StoryBookRepository;
+
+@SpringBootTest
+public class Test_ReadStoryBook2 {
+	
+	@Autowired
+	private StoryBookRepository storyBookRepository;
+	
+	@Test
+	public void read() {
+		
+		List<StoryBook> storyBooks = storyBookRepository.findAll();
+		
+		storyBooks.forEach(sb -> {
+			System.out.printf("書名: %s 作者: %s%n", sb.getName(), sb.getAuthor().getName());
+		});
+		
+	}
+	
+}
