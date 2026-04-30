9e01438a99f87446b8fc3a1d3fade5918fdfb615
User <User@host.docker.internal>
2026-04-30 20:33:10 +0800

    proxy

--- springboot-proxy-aop/src/test/java/com/example/demo/proxy/Test_Person.java
+++ springboot-proxy-aop/src/test/java/com/example/demo/proxy/Test_Person.java
@@ -9,8 +9,15 @@ public class Test_Person {
 	@Test
 	public void test() {
 		// 一般用法
-		Person man = new Man();
-		Person woman = new Woman();
+		//Person man = new Man();
+		//Person woman = new Woman();
+		//man.work();
+		//woman.work();
+		
+		// 使用代理模式
+		Person man = new PersonProxy(new Man());
+		Person woman = new PersonProxy(new Woman());
+		
 		man.work();
 		woman.work();
 	}
