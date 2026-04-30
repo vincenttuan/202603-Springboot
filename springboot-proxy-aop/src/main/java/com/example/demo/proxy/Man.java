0bd4543fc5cfdc497ee65a1cf3cff28e27ddcacc
User <User@host.docker.internal>
2026-04-30 20:13:43 +0800

    proxy

--- springboot-proxy-aop/src/main/java/com/example/demo/proxy/Man.java
+++ springboot-proxy-aop/src/main/java/com/example/demo/proxy/Man.java
@@ -4,8 +4,15 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯 
+		System.out.println("出門");
+		System.out.println("戴口罩");
+		
 		// 業務邏輯
 		System.out.println("Man 上班工作");
+		
+		// 公用邏輯
+		System.out.println("回家脫口罩");
 	}
 
 }
--- springboot-proxy-aop/src/main/java/com/example/demo/proxy/Woman.java
+++ springboot-proxy-aop/src/main/java/com/example/demo/proxy/Woman.java
@@ -4,8 +4,15 @@ public class Woman implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯
+		System.out.println("出門");
+		System.out.println("戴口罩");
+		
 		// 業務邏輯
 		System.out.println("Woman 出門買菜");
+		
+		// 公用邏輯
+		System.out.println("回家脫口罩");
 	}
 
 }
