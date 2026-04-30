1718742ddde2f6bd13123d210af3982c69b52122
User <User@host.docker.internal>
2026-04-30 19:26:37 +0800

    jpa

---
+++ springboot-jpa-storybook/src/test/java/com/example/demo/drink/Test_DrinkShopRead.java
@@ -0,0 +1,27 @@
+package com.example.demo.drink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class Test_DrinkShopRead {
+	
+	@Test
+	@Transactional
+	public void read() {
+		/**
+		 * 題目:
+		 * 起查詢所有飲料商品並印出
+		 * 
+		 * 1. 飲料名稱
+		 * 2. 飲料價格
+		 * 3. 所屬分類
+		 * 4. 飲料詳細資料: 熱量, 甜度, 冰塊
+		 * 5. 此飲料可搭配的加料
+		 * 
+		 * */
+		
+	}
+	
+}
