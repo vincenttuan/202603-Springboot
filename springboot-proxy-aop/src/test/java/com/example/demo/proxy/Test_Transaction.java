6bc7f7cdfa81afd7aa3681a2123f22c1bb13b492
User <User@DESKTOP-FM2EGTO>
2026-05-05 20:22:57 +0800

    proxy

---
+++ springboot-proxy-aop/src/test/java/com/example/demo/proxy/Test_Transaction.java
@@ -0,0 +1,21 @@
+package com.example.demo.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test_Transaction {
+
+	@Test
+	public void test() {
+		
+		AccountService proxy = (AccountService)new TransactionProxy(new AccountServiceImpl());
+		
+		proxy.transfer("小明", "小華", 5000);
+		
+		System.out.println("-----------------");
+		
+		proxy.transfer("小明", "小華", 20000);
+	}
+	
+}
