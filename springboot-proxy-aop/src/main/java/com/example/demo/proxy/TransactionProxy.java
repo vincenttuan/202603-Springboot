ab5ac71a188622d69b3cf10ae882d61ece533126
User <User@DESKTOP-FM2EGTO>
2026-05-05 20:16:50 +0800

    proxy

---
+++ springboot-proxy-aop/src/main/java/com/example/demo/proxy/TransactionProxy.java
@@ -0,0 +1,44 @@
+package com.example.demo.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class TransactionProxy {
+	
+	private Object object;
+	
+	public TransactionProxy(Object object) {
+		this.object = object;
+	}
+	
+	public Object getProxy() {
+		
+		ClassLoader loader = object.getClass().getClassLoader();
+		Class<?>[] interfaces = object.getClass().getInterfaces();
+		
+		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			Object result = null;
+			try {
+				TransactionAspect.begin();
+				
+				result = method.invoke(object, args);
+				
+				TransactionAspect.commit();
+				
+			} catch (Exception e) {
+				TransactionAspect.rollback();
+				System.out.println("錯誤原因:" + e.getMessage());
+			} finally {
+				TransactionAspect.close();
+			}
+			
+			return result;
+			
+		};
+		
+		return Proxy.newProxyInstance(loader, interfaces, handler);
+		
+	}
+	
+}
