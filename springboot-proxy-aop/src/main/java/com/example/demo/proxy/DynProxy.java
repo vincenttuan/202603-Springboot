17dd665a17afe4d0d259e47108be3d4a03a8b0c8
User <User@DESKTOP-FM2EGTO>
2026-05-05 19:17:03 +0800

    proxy

--- springboot-proxy-aop/src/main/java/com/example/demo/proxy/DynProxy.java
+++ springboot-proxy-aop/src/main/java/com/example/demo/proxy/DynProxy.java
@@ -1,5 +1,8 @@
 package com.example.demo.proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
 // 動態代理
 // 可以代理任何物件
 public class DynProxy {
@@ -16,14 +19,16 @@ public class DynProxy {
 		Object proxyObj = null;
 		
 		// 1. ClassLoader loader => 類別載入器
-		
+		ClassLoader loader = object.getClass().getClassLoader();
 		
 		// 2. Class<?>[] interfaces => 被代理對象所實作的介面
-		
+		Class<?>[] interfaces = object.getClass().getInterfaces();
 		
 		// 3. InvocationHandler handler => 處理代理的實現
+		InvocationHandler handler = null;
 		
-		
+		// 4. 建立代理實體
+		proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
 		return proxyObj;
 	}
 	
