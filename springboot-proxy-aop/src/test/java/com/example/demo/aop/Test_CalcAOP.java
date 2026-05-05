422b4f4904ce1a48dcfd70a13a1fc3ef1b033e4e
User <User@DESKTOP-FM2EGTO>
2026-05-05 21:16:36 +0800

    aop

---
+++ springboot-proxy-aop/src/test/java/com/example/demo/aop/Test_CalcAOP.java
@@ -0,0 +1,25 @@
+package com.example.demo.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.proxy.Calc;
+
+@SpringBootTest
+public class Test_CalcAOP {
+	
+	@Autowired
+	private Calc calc;
+	
+	@Test
+	public void test() {
+		Integer result1 = calc.add(20, 10);
+		System.out.println("加法結果: " + result1);
+		
+		Integer result2 = calc.div(20, 10);
+		System.out.println("除法結果: " + result2);
+		
+	}
+	
+}
