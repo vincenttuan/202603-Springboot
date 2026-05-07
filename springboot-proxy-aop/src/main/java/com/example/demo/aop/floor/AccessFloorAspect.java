e23b500eddbbaf0a764d99b514da5c1ee32e1f4f
User <User@host.docker.internal>
2026-05-07 20:36:24 +0800

    aop

--- springboot-proxy-aop/src/main/java/com/example/demo/aop/floor/AccessFloorAspect.java
+++ springboot-proxy-aop/src/main/java/com/example/demo/aop/floor/AccessFloorAspect.java
@@ -1,7 +1,12 @@
 package com.example.demo.aop.floor;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.service.floor.AccessController;
 
 /**
  * 寫一個通知
@@ -12,6 +17,24 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class AccessFloorAspect {
 	
+	@Autowired
+	private AccessController accessController;
 	
-	
+	@Around(value = "execution(* com.example.demo.service.floor.FloorService.enterFloor(..))")
+	public Object checkAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object result = null;
+		
+		Object[] args = joinPoint.getArgs();
+		String username = (String)args[0];
+		int floor = (int)args[1];
+		
+		if(accessController.hasAccess(username, floor)) {
+			System.out.printf("[AOP] %s 授權進入 %d 樓%n", username, floor);
+			result = joinPoint.proceed();
+		} else {
+			System.err.printf("[AOP] %s 未授權進入 %d 樓%n", username, floor);
+			throw new SecurityException("未授權進入該樓層");
+		}
+		return result;
+	}
 }
