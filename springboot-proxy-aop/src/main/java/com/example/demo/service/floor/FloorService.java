99cf3e4fcd4ba2a26e8e3bd0f2b9238a5d9093d9
User <User@host.docker.internal>
2026-05-07 20:11:33 +0800

    floor

---
+++ springboot-proxy-aop/src/main/java/com/example/demo/service/floor/FloorService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.floor;
+
+/**
+ * 門禁管制
+ * 1. 每一個用戶只能到有授權的樓層
+ * 2. 當用戶進樓層時 AOP 會檢查授權
+ * 3. 未授權進入的用戶, AOP 會攔截與拒絕
+ * */
+public interface FloorService {
+	String ebterFloor(String username, int floor);
+}
