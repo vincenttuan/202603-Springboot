061e09b7b2626d834cffc60b9d5427c812dbc9d9
User <User@host.docker.internal>
2026-05-12 20:00:55 +0800

    java

--- javasecurity/src/javasecurity/encryption/AESSample.java
+++ javasecurity/src/javasecurity/encryption/AESSample.java
@@ -1,7 +1,14 @@
 package javasecurity.encryption;
 
+// AES 對稱式加密
 public class AESSample {
+	
+	// 建立一個 AES Key (256bits, 32bytes)
+	private static final String KEY = "012345678901234567890123456789AB"; // 32 個字
+	
 	public static void main(String[] args) {
+		
+		
 		
 	}
 }
