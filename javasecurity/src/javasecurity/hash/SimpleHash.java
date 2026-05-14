61449d1a5e6dafda7c86ebe9816dd41bed0de6aa
User <User@host.docker.internal>
2026-05-14 19:22:26 +0800

    hash

---
+++ javasecurity/src/javasecurity/hash/SimpleHash.java
@@ -0,0 +1,31 @@
+package javasecurity.hash;
+
+import java.security.MessageDigest;
+import java.util.Base64;
+
+// Hash 雜湊
+public class SimpleHash {
+	public static void main(String[] args) throws Exception {
+		// 密碼
+		String password = "12345678";
+		
+		// 建立 SHA-256 雜湊演算法物件
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		
+		// 將字串轉 byte[] 後進行雜湊
+		byte[] hashBytes = md.digest(password.getBytes());
+		
+		// 將 byte[] 轉 16 進位格式的字串
+		StringBuilder sb = new StringBuilder();
+		for(byte b : hashBytes) {
+			sb.append(String.format("%02x", b));
+		}
+		
+		// 雜湊結果
+		String hashValue = sb.toString();
+		
+		System.out.printf("原始密碼: %s%n", password);
+		System.out.printf("雜湊結果: %s%n", hashValue);
+		
+	}
+}
