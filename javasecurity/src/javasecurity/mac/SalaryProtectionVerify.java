9825f8eea24c4632c7e4584d0da5c50b8b9dfb4e
User <User@host.docker.internal>
2026-05-19 19:27:45 +0800

    mac

--- javasecurity/src/javasecurity/mac/SalaryProtectionVerify.java
+++ javasecurity/src/javasecurity/mac/SalaryProtectionVerify.java
@@ -4,7 +4,7 @@ package javasecurity.mac;
  * 員工已知或得到的資訊
  * 薪資檔案位置   : src/javasecurity/mac/my_salary.txt
  * macKey檔案位置: src/javasecurity/mac/macKey.key
- * HR 發佈的 macHexValue:31846399dc0095accb10364c41bbe55a7a9479bb2c67f1e96d8653069319ff22
+ * HR 發佈的 macHexValue:1e35d4182d8efac6f67f82d2727f7ea393981b02291debc871c740911f3ecb4f
  * 
  * 如何得知 my_salary.txt 是由 HR 所發佈的 ? (來源端確認)
  * */
