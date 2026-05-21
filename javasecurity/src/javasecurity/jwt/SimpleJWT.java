8b674ac03cf71fb334a84e37d52a35fac47a5da8
User <User@host.docker.internal>
2026-05-21 19:27:34 +0800

    jwt

--- javasecurity/src/javasecurity/jwt/SimpleJWT.java
+++ javasecurity/src/javasecurity/jwt/SimpleJWT.java
@@ -31,8 +31,25 @@ public class SimpleJWT {
 		// 3. 進行簽名(將 claimSet 進行簽名) 的到 token(JWT)
 		String token = KeyUtil.signJWT(claimsSet, signingSecret);
 		System.out.printf("Token(JWT): %s%n", token);
-				
 		
+		System.out.println("\n=========================\n");
 		
+		// 4. 驗證 token(JWT)
+		boolean check = KeyUtil.verifyJWTSignature(token, signingSecret);
+		if(check) {
+			System.out.println("驗證成功");
+			// 讀取 token 中的 payload
+			JWTClaimsSet claims = KeyUtil.getClaimsFromToken(token);
+			System.out.printf("讀取 subject: %s%n", claims.getSubject());
+			System.out.printf("讀取 issure: %s%n", claims.getIssuer());
+			System.out.printf("讀取 id: %s%n", claims.getStringClaim("id"));
+			System.out.printf("讀取 name: %s%n", claims.getStringClaim("name"));
+			System.out.printf("讀取 dept: %s%n", claims.getStringClaim("dept"));
+			System.out.printf("讀取 floor: %s%n", claims.getStringClaim("floor"));
+			System.out.printf("讀取 tel: %s%n", claims.getStringClaim("tel"));
+			
+		} else {
+			System.err.println("驗證失敗");
+		}
 	}
 }
