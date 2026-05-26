9fb529c7d67223e20309802659cef4a44e35d69d
User <User@host.docker.internal>
2026-05-26 21:29:20 +0800

    spring

---
+++ springboot-security/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Spring Security 設定類別
+ * - 設定表單登入, 授權規則, 登出行為, 例外處理與用戶資料
+ * */
+
+@Configuration // 標註為 Spring 設定類別
+@EnableWebSecurity // 啟用 Spring Web Security
+public class SecurityConfig {
+	
+	
+}
