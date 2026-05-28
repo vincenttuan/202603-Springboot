4610bc4023ed822b3830e91f15ea0dcb6f5a6c9a
User <User@host.docker.internal>
2026-05-28 20:31:37 +0800

    spring

---
+++ springboot-security-mysql/src/main/java/com/example/demo/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.entity.AppUser;
+import com.example.demo.repository.AppUserRepository;
+
+@Configuration
+public class DataInitializer {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 預設先在資料表新增一個 admin 使用者
+	@Bean
+	public CommandLineRunner initAdmin(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if(!appUserRepository.existsByUsername("admin")) {
+				AppUser admin = new AppUser();
+				admin.setUsername("admin");
+				admin.setPassword(passwordEncoder.encode("5678"));
+				admin.setRole("ADMIN");
+				admin.setEnabled(true);
+				
+				appUserRepository.save(admin);
+			}
+		};
+	}
+	
+}
