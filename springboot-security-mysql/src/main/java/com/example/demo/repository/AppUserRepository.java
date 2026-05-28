56e57552c40954d324486bd162460a946b6d0af1
User <User@host.docker.internal>
2026-05-28 20:07:00 +0800

    spring

---
+++ springboot-security-mysql/src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.AppUser;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+	
+	Optional<AppUser> findByUsername(String username);
+	
+	boolean exitsByUsername(String username);
+}
