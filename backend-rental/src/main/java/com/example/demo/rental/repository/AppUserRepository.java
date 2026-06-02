f15ab9541783d4d04ea5926a6087202674c9a23f
User <User@host.docker.internal>
2026-06-02 21:18:41 +0800

    rental

---
+++ backend-rental/src/main/java/com/example/demo/rental/repository/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.rental.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.rental.model.entity.AppUser;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+	Optional<AppUser> findByUsername(String username);
+	boolean existsByUsername(String username);
+}
