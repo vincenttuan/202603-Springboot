2079d83ae2bf44a4c0aaff07f6ee42f6fcdb1daf
User <User@host.docker.internal>
2026-06-02 20:03:08 +0800

    rental

---
+++ backend-rental/src/main/java/com/example/demo/BackendRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRentalApplication.class, args);
+	}
+
+}
