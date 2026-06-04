c49193e72e10b962a12bbe5f96314e3625e38f7c
User <User@host.docker.internal>
2026-06-04 19:45:08 +0800

    apiResponse

---
+++ backend-rental/src/main/java/com/example/demo/rental/model/dto/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.demo.rental.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+	private int status;
+	private String message;
+	private T data;
+	
+	public static <T> ApiResponse<T> success(String message, T data) {
+		return new ApiResponse<>(200, message, data);
+	}
+	
+	public static <T> ApiResponse<T> created(String message, T data) {
+		return new ApiResponse<>(201, message, data);
+	}
+	
+	public static <T> ApiResponse<T> error(int status, String message) {
+		return new ApiResponse<>(status, message, null);
+	}
+}
