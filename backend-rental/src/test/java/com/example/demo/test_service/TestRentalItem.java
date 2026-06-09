0424262b3ae7cd8f72ada9afb7855e37281522dd
User <User@host.docker.internal>
2026-06-09 19:57:08 +0800

    test

--- backend-rental/src/test/java/com/example/demo/test_service/TestRentalItem.java
+++ backend-rental/src/test/java/com/example/demo/test_service/TestRentalItem.java
@@ -1,15 +1,19 @@
 package com.example.demo.test_service;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.rental.exception.ResourceNotFoundException;
+import com.example.demo.rental.model.dto.item.RentalItemRequest;
 import com.example.demo.rental.model.dto.item.RentalItemResponse;
+import com.example.demo.rental.model.enums.ItemStatus;
 import com.example.demo.rental.service.RentalItemService;
 
 @SpringBootTest
-public class FindRentalItem {
+public class TestRentalItem {
 	
 	@Autowired
 	private RentalItemService rentalItemService;
@@ -25,7 +29,7 @@ public class FindRentalItem {
 		
 	}
 	
-	@Test
+	//@Test
 	public void findById() {
 		Long id = 1L;
 		try {
@@ -37,6 +41,21 @@ public class FindRentalItem {
 			System.err.println(e);
 		}
 		
+	}
+	
+	@Test
+	public void create() {
+		RentalItemRequest request = new RentalItemRequest();
+		request.setName("椅子");
+		request.setStatus(ItemStatus.AVAILABLE);
+		request.setType("家具");
+		request.setLocation("倉庫");
+		request.setPricePerHour(new BigDecimal("5"));
+		request.setDescription("人體工學");
+		request.setImageUrl("https://enjoycaster.com.tw/wp-content/uploads/2021/12/%E6%A4%8D%E5%B9%B8%E7%A6%8F-%E6%AD%A3-%E5%8E%BB%E8%83%8C-%E9%BB%91-600x900.jpg");
+		
+		var rentalItemResponse = rentalItemService.create(request);
+		System.out.println("新增成功: " + rentalItemResponse);
 	}
 	
 }
