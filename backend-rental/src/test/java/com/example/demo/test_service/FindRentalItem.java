54d2776a0a4532964798f8bc95a9ae17af97fe59
User <User@host.docker.internal>
2026-06-09 19:40:45 +0800

    test

--- backend-rental/src/test/java/com/example/demo/test_service/FindRentalItem.java
+++ backend-rental/src/test/java/com/example/demo/test_service/FindRentalItem.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.rental.exception.ResourceNotFoundException;
+import com.example.demo.rental.model.dto.item.RentalItemResponse;
 import com.example.demo.rental.service.RentalItemService;
 
 @SpringBootTest
@@ -12,7 +14,7 @@ public class FindRentalItem {
 	@Autowired
 	private RentalItemService rentalItemService;
 	
-	@Test
+	//@Test
 	public void find() {
 		String keyword = null; // "電腦", "4k"
 		String type = null; // "教室", "設備" , "場地"
@@ -20,6 +22,20 @@ public class FindRentalItem {
 		
 		System.out.println("筆數: " + list.size());
 		list.forEach(System.out::println);
+		
+	}
+	
+	@Test
+	public void findById() {
+		Long id = 1L;
+		try {
+			var rentalItemResponse = rentalItemService.findById(id);
+			System.out.println(rentalItemResponse);
+		} catch (ResourceNotFoundException e) {
+			System.err.println(e);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 		
 	}
 	
