11a1d57657b1d4e271e73bbe503f9ab3ba2541c3
User <User@host.docker.internal>
2026-06-16 20:28:15 +0800

    test

---
+++ backend-rental/src/test/java/com/example/demo/test_controller/AuthControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.test_controller;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.rental.model.dto.auth.RegisterRequest;
+import com.example.demo.rental.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Test
+	public void register() throws Exception {
+		
+		String json = """
+				{
+				   "username": "user4",
+				   "password": "444444",
+				   "fullName": "陳曉明",
+				   "phone": "0944444444"
+				}
+				""";
+		
+		mockMvc.perform(post("/api/auth/register")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+				.andExpect(status().isOk());
+		
+		// 驗證 AuthService.register() 真的有被呼叫到一次
+		//verify(authService, times(1)).register((RegisterRequest) any(RegisterRequest.class));
+	}
+}
