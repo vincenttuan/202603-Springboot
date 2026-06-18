ae4526ec988254626945b20202304b85cf7b3083
User <User@host.docker.internal>
2026-06-18 19:22:48 +0800

    test

---
+++ backend-rental/src/test/java/com/example/demo/test_controller/RentalItemControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.demo.test_controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.example.demo.rental.service.RentalItemService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RentalItemControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private RentalItemService rentalItemService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Test
+	public void findAll() throws Exception {
+		
+		MvcResult result = mockMvc.perform(get("/api/items"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		String responseBody = result.getResponse().getContentAsString();
+		
+		System.out.println(responseBody);
+				
+				
+		
+	}
+	
+}
