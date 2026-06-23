51b8d76380c747ee424d29228a26825e6a5e3f2c
User <User@host.docker.internal>
2026-06-23 20:00:17 +0800

    log

---
+++ backend-rental/src/main/java/com/example/demo/rental/aop/LoggingAspect.java
@@ -0,0 +1,37 @@
+package com.example.demo.rental.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.hibernate.internal.log.IncubationLogger_.logger;
+import org.springframework.stereotype.Component;
+import com.example.demo.rental.repository.RentalItemRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    private final RentalItemRepository rentalItemRepository;
+
+    LoggingAspect(RentalItemRepository rentalItemRepository) {
+        this.rentalItemRepository = rentalItemRepository;
+    }
+	
+	@Around("execution(* com.example.demo.rental.controller..*(..)) || execution(* com.example.demo.rental.service..*(..))")
+	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		String methodName = joinPoint.getSignature().toShortString();
+		
+		try {
+			Object result = joinPoint.proceed();
+			long diff = System.currentTimeMillis() - start;
+			log.info("{} 執行成功, 耗時 {} ms", methodName, diff);
+		} catch (Throwable ex) {
+			long diff = System.currentTimeMillis() - start;
+			log.warn("{} 執行失敗, 耗時 {} ms, 原因: {}", methodName, diff, ex.getMessage());
+			throw ex;
+		}
+	}
+}
