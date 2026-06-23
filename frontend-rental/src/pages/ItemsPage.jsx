54260fb77c9218528a6f2309a9ed9a6a45309613
User <User@host.docker.internal>
2026-06-23 21:08:50 +0800

    react

---
+++ frontend-rental/src/pages/ItemsPage.jsx
@@ -0,0 +1,12 @@
+function ItemsPage() {
+    return (
+        <section>
+            <h1>租用項目</h1>
+            <p>這裡會顯示可租用的教室, 設備與場地</p>
+        </section>
+
+    )
+
+}
+
+export default ItemsPage;
