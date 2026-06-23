2c438701e4610809124ed2f26f6c73c339de0c22
User <User@host.docker.internal>
2026-06-23 21:08:42 +0800

    react

--- frontend-rental/src/main.jsx
+++ frontend-rental/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ItemsPage from './pages/ItemsPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ItemsPage />
   </StrictMode>,
 )
