90e6af2fd3932b6070a78c5262adb7bb693d10fe
User <User@host.docker.internal>
2026-06-23 20:37:38 +0800

    react

---
+++ frontend-rental/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      {/* 頁面主標題 */}
+      <h1>租用預約系統</h1>
+
+      {/* 頁面說明文字 */}
+      <p>歡迎使用 React 租用預約系統</p>
+    </div>
+  )
+
+}
+
+export default App
