fa62916a709a4d02702de10655505c3a790cd836
User <User@host.docker.internal>
2026-06-23 21:37:11 +0800

    react

--- frontend-rental/src/main.jsx
+++ frontend-rental/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import React from 'react'
 // BrowserRouter 讓 React 可以使用前端路由(網頁路徑切換)
 import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
